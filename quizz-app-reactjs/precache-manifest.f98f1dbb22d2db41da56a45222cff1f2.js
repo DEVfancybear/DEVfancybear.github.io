91e2219d3ce247d95f3e038ea9112449c87ea9c6
DEVfancybear <duongltgch18233@fpt.edu.vn>
2019-09-17 14:31:02 +0700

    update quiz reactjs app

---
+++ quizz-app-reactjs/precache-manifest.f98f1dbb22d2db41da56a45222cff1f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b32074f7dd19b03129a235d3626ade4",
+    "url": "/quizz-app-reactjs/index.html"
+  },
+  {
+    "revision": "3937cbdc79a5869a3cad",
+    "url": "/quizz-app-reactjs/static/css/main.6bd65d50.chunk.css"
+  },
+  {
+    "revision": "5a25410b6b263d0ca517",
+    "url": "/quizz-app-reactjs/static/js/2.4f612e9c.chunk.js"
+  },
+  {
+    "revision": "3937cbdc79a5869a3cad",
+    "url": "/quizz-app-reactjs/static/js/main.153e3c3d.chunk.js"
+  },
+  {
+    "revision": "19022503978dcd6fce2a",
+    "url": "/quizz-app-reactjs/static/js/runtime~main.5af3ad1b.js"
+  }
+]);
